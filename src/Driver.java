ad21424f1a72689a1d38dca4f25fbb7c16d5dd1b
gerald <greengorilla3@protonmail.com>
2021-03-05 22:58:07 -0800

    Added random backgrounds

--- src/Driver.java
+++ src/Driver.java
@@ -23,25 +23,24 @@ public class Driver extends Application {
     public void start(Stage window) throws Exception {
 
         //get paper image from resources, and pass it to an ImageView.
-        //Image background = new Image(
-        //        getClass().getResource("resources/screen1.jpg").toString()
-       // );
-       // ImageView paperView = new ImageView(background);
-        //paperView.setPreserveRatio(true);
+        Image aiBackground = new Image(
+                getClass().getResource("resources/backgrounds/screen" +
+                        (int)(Math.random() * (14) + 1) + ".jpg").toString()
+        );
+        ImageView paperView = new ImageView(aiBackground);
+        paperView.setFitWidth(400);
+        paperView.setFitHeight(400);
 
         window.setTitle("SaveScummer");
-
-        JFileChooser fileChooser = new JFileChooser(".");
-        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
 
         button = new Button("Click me");
 
         StackPane layout = new StackPane();
         layout.getChildren().add(button);
 
-        //Group root = new Group(paperView);
+        Group root = new Group(paperView);
 
-        Scene scene = new Scene(layout, 300, 250);
+        Scene scene = new Scene(root, 400, 400);
         window.setScene(scene);
         window.show();
     }
